2286bffe10d7072a326fad7ddfbee653afb2160e
Dinesh Solanki g <77187635+Dinesh-Solanki@users.noreply.github.com>
2024-01-12 15:42:16 +0530

    Add files via upload

---
+++ Order_Management/Order_Management/DAO/IOrderManagementRepository.cs
@@ -0,0 +1,24 @@
+﻿using Order_Management.Entity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_Management.DAO
+{
+    internal interface IOrderManagementRepository
+    {
+        bool createUser(User user);
+        bool createProduct(User user, Products product);
+
+        List<Products> getAllProducts();
+        List<Tuple<Orders, Products>> getOrderByUser(User user);
+
+        bool cancelOrder(int userId, int orderId);
+
+        bool createOrder(User user, List<Products> product);
+
+    }
+}
---
+++ Order_Management/Order_Management/Util/DBConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_Management.Util
+{
+    internal class DBConnection
+    {
+        static SqlConnection connection = null;
+
+        public static SqlConnection GetConnection()
+        {
+            connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["AppConnection"].ConnectionString;
+            return connection;
+        }
+    }
+}
